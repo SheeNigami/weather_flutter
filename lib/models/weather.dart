dac61e77d446629096a45ddd6ee84ae498bee142
shini <sheenhern@outlook.com>
2020-01-14 13:14:28 +0800

    Weather Model done

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -15,6 +15,53 @@ enum WeatherCondition {
 }
 
 class Weather extends Equatable {
+  final WeatherCondition condition;
+  final String formattedCondition;
+  final double minTemp;
+  final double temp;
+  final double maxTemp;
+  final int locationId;
+  final String created;
+  final DateTime lastUpdated;
+  final String location;
+
+  Weather({
+    this.condition,
+    this.formattedCondition,
+    this.minTemp,
+    this.temp,
+    this.maxTemp,
+    this.locationId,
+    this.created,
+    this.lastUpdated,
+    this.location,
+  }) : super([
+          condition,
+          formattedCondition,
+          minTemp,
+          temp,
+          maxTemp,
+          locationId,
+          created,
+          lastUpdated,
+          location,
+  ]);
+
+  static Weather fromJson(dynamic json) {
+    final consolidatedWeather = json['consolidated_weather'][0];
+    return Weather(
+      condition: _mapStringToWeatherCondition(
+          consolidatedWeather['weather_state_abbr']),
+      formattedCondition: consolidatedWeather['weather_state_name'],
+      minTemp: consolidatedWeather['min_temp'] as double,
+      temp: consolidatedWeather['the_temp'] as double,
+      maxTemp: consolidatedWeather['max_temp'] as double,
+      locationId: json['woeid'] as int,
+      created: consolidatedWeather['created'],
+      lastUpdated: DateTime.now(),
+      location: json['title'],
+    );
+  }
 
   static WeatherCondition _mapStringToWeatherCondition(String weatherString) {
     WeatherCondition state;
