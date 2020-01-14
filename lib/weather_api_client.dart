ae104aa0d4efc3a08b2d3bfe727568d057fe2cc1
shini <sheenhern@outlook.com>
2020-01-14 14:15:23 +0800

    WeatherApi getWeather()

---
+++ lib/weather_api_client.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_weather/models/weather.dart';
+
+class WeatherApi {
+  static const baseUrl = 'https://www.metaweather.com';
+  final http.Client httpClient;
+
+  WeatherApi({@required this.httpClient}) : assert(httpClient != null);
+
+  Future<int> getLocationId(String city) async {
+    final locationUrl = '$baseUrl/api/location/search/?query=$city';
+    final locationResponse = await this.httpClient.get(locationUrl);
+    if (locationResponse.statusCode != 200) {
+      throw Exception('error getting location id');
+    }
+
+    final locationJson = jsonDecode(locationResponse.body) as List;
+    return (locationJson.first)['woeid'];
+  }
+
+  Future<Weather> fetchWeather(int locationID) async {
+    final weatherUrl = '$baseUrl/api/location/$locationID';
+    final weatherResponse = await this.httpClient.get(weatherUrl);
+
+    if (weatherResponse.statusCode != 200) {
+      throw Exception('error fetching weather for location');
+    }
+
+    final weatherJson = jsonDecode(weatherResponse.body) as List;
+    return Weather.fromJson(weatherJson);
+  }
+
+  Future<Weather> getWeather(String city) async {
+    final int locationId = await getLocationId(city);
+    return fetchWeather(locationId);
+  }
+}
