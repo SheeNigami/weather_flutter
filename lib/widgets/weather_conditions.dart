2b629486eabeae50eb873e4f5a04dcc760f1a734
shini <sheenhern@outlook.com>
2020-01-14 19:25:42 +0800

    Reduce weather icon size

--- lib/widgets/weather_conditions.dart
+++ lib/widgets/weather_conditions.dart
@@ -19,27 +19,51 @@ class WeatherConditions extends StatelessWidget {
     Image image;
     switch (condition) {
       case WeatherCondition.clear:
-        image = Image.asset('assets/clear.png');
+        image = Image.asset(
+          'assets/clear.png',
+           width: 192,
+           height: 192,
+        );
         break;
       case WeatherCondition.lightCloud:
       case WeatherCondition.heavyCloud:
-        image = Image.asset('assets/cloudy.png');
+        image = Image.asset(
+          'assets/cloudy.png',
+           width: 192,
+           height: 192,
+        );
         break;
       case WeatherCondition.hail:
       case WeatherCondition.snow:
       case WeatherCondition.sleet:
-        image = Image.asset('assets/snow.png');
+        image = Image.asset(
+          'assets/snow.png',
+           width: 192,
+           height: 192,
+        );
         break;
       case WeatherCondition.heavyRain:
       case WeatherCondition.lightRain:
       case WeatherCondition.showers:
-        image = Image.asset('assets/rainy.png');
+        image = Image.asset(
+          'assets/rainy.png',
+           width: 192,
+           height: 192,
+        );
         break;
       case WeatherCondition.thunderstorm:
-        image = Image.asset('assets/thunderstorm.png');
+        image = Image.asset(
+          'assets/thunderstorm.png',
+           width: 192,
+           height: 192,
+        );
         break;
       case WeatherCondition.unknown:
-        image = Image.asset('assets/clear.png');
+        image = Image.asset(
+          'assets/clear.png',
+           width: 192,
+           height: 192,
+        );
         break;
     }
     return image;
