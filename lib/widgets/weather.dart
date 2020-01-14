a7beae41ea09303a33a4280112f79f0f9e19fab7
shini <sheenhern@outlook.com>
2020-01-14 17:52:56 +0800

    Finished Basic Version

--- lib/widgets/weather.dart
+++ lib/widgets/weather.dart
@@ -7,3 +7,97 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_weather/widgets/widgets.dart';
 import 'package:flutter_weather/weather_api.dart';
 import 'package:flutter_weather/blocs/blocs.dart';
+
+class WeatherWidget extends StatefulWidget {
+  final WeatherApi weatherApi;
+
+  WeatherWidget({Key key, @required this.weatherApi})
+      : assert(weatherApi != null),
+        super(key: key);
+
+  @override 
+  State<WeatherWidget> currentState() => _weatherState();
+}
+
+class _WeatherWidgetState extends State<WeatherWidget> {
+  WeatherBloc _weatherBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherBloc = WeatherBloc(weatherApi: widget.weatherApi);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Weather'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              final city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CitySelection(),
+                ),
+              );
+              if (city != null) {
+                _weatherBloc.dispatch(FetchWeather(city: city));
+              }
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: BlocBuilder(
+          bloc: _weatherBloc,
+          builder: (_, WeatherState state) {
+            if (state is WeatherEmpty) {
+              return Center(child: Text('Please Select a Location'));
+            }
+            if (state is WeatherLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (state is WeatherLoaded) {
+              final weather = state.weather;
+
+              return ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 100.0),
+                    child: Center(
+                      child: Location(location: weather.location),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 50.0),
+                    child: Center(
+                      child: CombinedWeatherTemperature(
+                        weather: weather,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+            if (state is WeatherError) {
+              return Text(
+                'Something went wrong!',
+                style: TextStyle(color: Colors.red),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _weatherBloc.dispose();
+    super.dispose();
+  }
+}
+
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export './city_selection.dart';
+export './combined_weather_temp.dart';
+export './location.dart';
+export './temperature.dart';
+export './weather_conditions.dart';
+export './weather.dart';
