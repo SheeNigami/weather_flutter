361d2dee74917fe3bf87025b2328348d51d7cc18
shini <sheenhern@outlook.com>
2020-01-14 12:27:40 +0800

    Basic Weather Model

---
+++ lib/models/weather.dart
@@ -0,0 +1,57 @@
+import 'package:equatable/equatable.dart';
+
+enum WeatherCondition {
+  snow,
+  sleet,
+  hail,
+  thunderstorm,
+  heavyRain,
+  lightRain,
+  showers,
+  heavyCloud,
+  lightCloud,
+  clear,
+  unknown
+}
+
+class Weather extends Equatable {
+
+  static WeatherCondition _mapStringToWeatherCondition(String weatherString) {
+    WeatherCondition state;
+    switch(weatherString) {
+      case 'sn':
+        state = WeatherCondition.snow;
+        break;
+      case 'sl':
+        state = WeatherCondition.sleet;
+        break;
+      case 'h':
+        state = WeatherCondition.hail;
+        break;
+      case 't':
+        state = WeatherCondition.thunderstorm;
+        break;
+      case 'hr':
+        state = WeatherCondition.heavyRain;
+        break;
+      case 'lr':
+        state = WeatherCondition.lightRain;
+        break;
+      case 's':
+        state = WeatherCondition.showers;
+        break;
+      case 'hc':
+        state = WeatherCondition.heavyCloud;
+        break;
+      case 'lc':
+        state = WeatherCondition.lightCloud;
+        break;
+      case 'c':
+        state = WeatherCondition.clear;
+        break;
+      default:
+        state = WeatherCondition.unknown;
+    }
+    return state;
+  }
+}
